0582d651d2934002f364e20ce95a1c499ff8abf9
JoaoPauloCMarra <contato@webnexus.com.br>
2021-04-16 11:21:27 +0200

    spinner opt for loading & image loading comp

--- src/shared/Image.tsx
+++ src/shared/Image.tsx
@@ -1,27 +1,45 @@
 import { FunctionalComponent } from 'preact';
-import { memo, useState } from 'preact/compat';
+import { memo, useCallback, useEffect, useMemo, useState } from 'preact/compat';
+
 import { logError } from '../utils/logger';
 
 interface Props {
   src?: string;
   alt?: string;
+  height?: number | string;
+  width?: number | string;
   'data-testid'?: string;
 }
 
-const Error: FunctionalComponent<Props> = ({ src, alt = '...', 'data-testid': testId }) => {
+const Error: FunctionalComponent<Props> = ({ src, alt = '...', height, width, 'data-testid': testId }) => {
   const [loading, setLoading] = useState(true);
 
-  const onLoad = () => {
+  const onLoad = useCallback(() => {
     setLoading(false);
-  };
+  }, []);
 
-  const onError = (e: any) => {
-    logError('Image Loader', `image: ${src}\n`, e);
-  };
+  const onError = useCallback(
+    (e: any) => {
+      logError('Image Loader', `image: ${src}\n`, e);
+    },
+    [src],
+  );
+
+  useEffect(() => {
+    if (src) {
+      const preloader = new Image();
+      preloader.src = src;
+      preloader.onload = onLoad;
+      preloader.onerror = onError;
+    }
+  }, [src, onLoad, onError]);
+
+  const style = useMemo(() => ({ height, width }), [height, width]);
 
   return (
     <span className={`image-wrapper${loading ? ' hidden' : ''}`}>
-      <img data-testid={testId} src={src} alt={alt} onLoad={onLoad} onError={onError} />
+      {loading && <div className="image-preloader" style={style} />}
+      {!loading && <img data-testid={testId} src={src} alt={alt} style={style} />}
     </span>
   );
 };
--- src/shared/Loading.tsx
+++ src/shared/Loading.tsx
@@ -1,17 +1,54 @@
 import { FunctionalComponent } from 'preact';
-import { memo } from 'preact/compat';
+import { memo, useMemo } from 'preact/compat';
 
-const Loading: FunctionalComponent = () => (
-  <div className="loading-wrapper center">
-    <div className="loading-content">
-      <div className="loading-ellipsis">
-        <div />
-        <div />
-        <div />
-        <div />
-      </div>
+type LoadingTypes = 'dots' | 'spinner';
+
+interface Props {
+  type?: LoadingTypes;
+  color?: string;
+}
+
+const Loading: FunctionalComponent<Props> = ({ type = 'dots', color }) => {
+  const component = useMemo(() => {
+    const style = {
+      dots: {
+        background: color,
+      },
+      spinner: {
+        background: color,
+        borderColor: color,
+      },
+    };
+    const loadingTypes: { [key in LoadingTypes]: JSX.Element } = {
+      dots: (
+        <div className="loading-ellipsis">
+          <div style={style.dots} />
+          <div style={style.dots} />
+          <div style={style.dots} />
+          <div style={style.dots} />
+        </div>
+      ),
+      spinner: (
+        <div className="loading-spinner">
+          <div style={style.spinner} />
+          <div style={style.spinner} />
+          <div style={style.spinner} />
+          <div style={style.spinner} />
+        </div>
+      ),
+    };
+    try {
+      return loadingTypes[type];
+    } catch (error) {
+      return loadingTypes.dots;
+    }
+  }, [type, color]);
+
+  return (
+    <div className="loading-wrapper center">
+      <div className="loading-content">{component}</div>
     </div>
-  </div>
-);
+  );
+};
 
 export default memo(Loading);
